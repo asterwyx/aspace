a50842525771490ff68537a94e5fde205bdb1a7a
wangyixue <wangyixue@uniontech.com>
2022-08-15 14:45:40 +0800

    feat: Add install and uninstall support
    Refactor CMakeLists.txt, add install and uninstall support. Also now we
    can sun aspace and aspace-daemon from cli directly. There'are also a
    systemd unit service file installed, a dbus service file installed, a
    freedesktop entry file installed and a glib schema installed.
    
    Log: Add install and uninstall support
    Influence: build directory, system files

--- common/global.h
+++ common/global.h
@@ -4,11 +4,12 @@
 
 #pragma once
 
-#ifndef DBUS_SERVICE_NAME
+#if not defined(DBUS_SERVICE_NAME)
 #define DBUS_SERVICE_NAME               "org.deepin.aspace"
 #endif
 #define DBUS_WEATHER_INTERFACE_NAME     DBUS_SERVICE_NAME".Weather"
-#ifndef DBUS_PATH_PREFIX
+
+#if not defined(DBUS_PATH_PREFIX) 
 #define DBUS_PATH_PREFIX                "/org/deepin/aspace/"
 #endif
 #define DBUS_WEATHER_PATH               DBUS_PATH_PREFIX"weather"
